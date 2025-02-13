72303d6477a74d73d5e6d44827963280b4921ce3
CitizenOneX <17427385+CitizenOneX@users.noreply.github.com>
2025-02-13 15:24:45 +1100

    modified to handle exception from failure to connect

--- packages/frame_ble/examples/hello_world.py
+++ packages/frame_ble/examples/hello_world.py
@@ -3,16 +3,18 @@ from frame_ble import FrameBle
 
 async def main():
     frame = FrameBle()
-    await frame.connect()
 
-    if frame.is_connected():
+    try:
+        await frame.connect()
+
         # wait for a print() to ensure the Lua has executed, not just that the command was sent successfully
         await frame.send_lua("frame.display.text('Hello, Frame!', 1, 1);frame.display.show();print(nil)", await_print=True)
+
         await frame.disconnect()
 
-    else:
-        print("Not connected to Frame")
-
+    except Exception as e:
+        print(f"Not connected to Frame: {e}")
+        return
 
 if __name__ == "__main__":
     asyncio.run(main())
