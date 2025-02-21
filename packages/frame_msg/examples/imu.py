7f36c2206b89346ad228762512ca224ab4a2f5ff
CitizenOneX <17427385+CitizenOneX@users.noreply.github.com>
2025-02-22 00:40:50 +1100

    clean up comments and logging

--- packages/frame_msg/examples/imu.py
+++ packages/frame_msg/examples/imu.py
@@ -23,19 +23,11 @@ async def main():
         await frame.upload_frame_app(local_filename="lua/imu_frame_app.lua")
 
         # attach the print response handler so we can see stdout from Frame Lua print() statements
-        # If we assigned this handler before the frameside app was running,
-        # any await_print=True commands will echo the acknowledgement byte (e.g. "1"), but if we assign
-        # the handler now we'll see any lua exceptions (or stdout print statements)
         frame.attach_print_response_handler()
 
         # "require" the main frame_app lua file to run it, and block until it has started.
         # It signals that it is ready by sending something on the string response channel.
         await frame.start_frame_app()
-
-        # NOTE: Now that the Frameside app has started there is no need to send snippets of Lua
-        # code directly (in fact, we would need to send a break_signal if we wanted to because
-        # the main app loop on Frame is running).
-        # From this point we do message-passing with first-class types and send_message() (or send_data())
 
         # hook up the RxIMU receiver
         rx_imu = RxIMU(smoothing_samples=5)
@@ -47,7 +39,8 @@ async def main():
         for _ in range(1,100):
             # get the IMU update as soon as it arrives
             imu_update = await asyncio.wait_for(imu_queue.get(), timeout=10.0)
-            print(f"{imu_update}")
+            # note that raw eCompass values will need to be calibrated before use
+            print(f"pitch: {imu_update.pitch:.2f} roll: {imu_update.roll:.2f} compass: {imu_update.compass}")
 
         # Unsubscribe for IMU updates
         await frame.send_message(0x40, TxCode(value=0).pack())
